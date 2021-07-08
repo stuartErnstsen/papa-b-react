206631820d1930c628d41f8d8bb09eb82833a09b
stuartErnstsen <stuarternstsen@gmail.com>
2021-07-07 20:40:26 -0600

    Started building out layout for landing page and figuring out elements sizing

--- db/seed.sql
+++ db/seed.sql
@@ -3,6 +3,7 @@ create table pb_users (
     email varchar(150),
     first_name varchar(50),
     last_name varchar(50),
+    hashword varchar(2000),
     is_admin boolean default false
 );
 
--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -11,13 +11,21 @@ const Landing = (props) => {
     })
     return (
         <main className='landing'>
-            <button onClick={() => setShowCart(old => !old)}>click</button>
-            {`${showCart}`}
-            <section>
-                {cartContainerTransition((style, item) =>
-                    item ? <animated.div style={style} className='cart' /> : null
-                )}
+            <section className='item-container'>
+                <div className='item-inner-container'>
+                    <div className='item-inner-left'>
+                        <img src='https://cdn.shopify.com/s/files/1/1363/7665/products/30ml_bottle.png?v=1536367945' alt='juice' />
+                    </div>
+                    <div className='item-inner-right'>
+                        <h2>BRAD'S GNL</h2>
+                        <p>Do veniam reprehenderit laborum ipsum quis in dolore mollit excepteur. Duis non exercitation sit adipisicing tempor ullamco laboris sit labore magna exercitation ut. Pariatur ullamco laboris ut pariatur elit consectetur. Duis proident et elit adipisicing proident ea irure non aliqua commodo ullamco pariatur irure irure. Do nulla esse labore ipsum culpa pariatur ullamco dolore pariatur cupidatat ex.</p>
+                    </div>
+                </div>
+                <button className='buy-button' onClick={() => setShowCart(old => !old)}>BUY</button>
             </section>
+            {cartContainerTransition((style, item) =>
+                item ? <animated.div style={style} className='cart' /> : null
+            )}
         </main>
     )
 }
